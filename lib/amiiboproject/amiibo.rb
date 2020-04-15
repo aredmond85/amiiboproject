34351520df12fc107eaea200131f6f37cf724345
aredmond85 <aredmond85@gmail.com>
2020-04-15 17:32:47 -0500

    instance methods list_by_all_name & list_by_character created

--- lib/amiiboproject/amiibo.rb
+++ lib/amiiboproject/amiibo.rb
@@ -11,19 +11,15 @@ class Amiiboproject::Amiibo
     #create an instance of amiibo
     def self.create(array)
         array.each do |hash|
-            new(hash["character"],hash["gameSeries"],hash["name"])
+            amiibo = new(hash["character"],hash["gameSeries"],hash["name"])
+            amiibo.save 
         end
-    end
-
-    def self.find_by_character(input)
-        @@all.find_all {|i| i.character.downcase == input}
     end
 
     attr_accessor :character,:gameSeries, :name
 
     def initialize(character, gameSeries, name)
         @character, @gameSeries, @name = character, gameSeries, name
-        save
     end
 
     def save
--- lib/amiiboproject/cli.rb
+++ lib/amiiboproject/cli.rb
@@ -3,27 +3,61 @@ class Amiiboproject::CLI
     def start
         intro
         instruction_loop
+
     end
 
     def intro
+        puts "\n"
         puts "Welcome to the Amiibo Selection!"
+        puts "\n"
+    end
+
+    def gets_input(input)
+        input = gets.strip
+    end
+
+    #outputs the text to put in a valid option
+    def invalid_selection
+        puts "Please enter a valid option!"
+        puts "\n"
+    end  
+    
+    def list_by_all_name
+        Amiiboproject::Amiibo.all.each_with_index {|x, i| puts "#{i+1}. #{x.name}"}
+    end
+
+       #finds characters associated by the character name
+    def list_by_character(input)
+        Amiiboproject::Amiibo.all.select {|x| puts "#{x.name.capitalize}" if x.character.downcase == input}
+    end
+
+    #outputs the menu of options for the user
+    def menu
+        puts "Type in a Nintendo character to see a list of Amiibos associated with that character or all to see ALL Amiibos."
+        puts "Type 'exit' to exit the CLI"
+        puts "\n"
+        sleep(2)
     end
 
     def instruction_loop
+        puts "Compiling all Amiibo Results...one moment..."
+        sleep(1)
+        puts "\n"
+        Amiiboproject::Amiibo.create(Amiiboproject::APIManager.get_amiibo)
         menu
         input = nil 
         while input != "exit" 
             input = gets.strip.downcase
             case input
                 when "zelda"
-                    list_selected_character
-                    puts "Please enter another character or type 'menu' for menu: \n\n"
+                    list_by_character(input)
+                    puts "Please enter another character or type 'menu' for menu: \n"
                 when "mario"
-                    list_selected_character
-                    puts "Please enter another character or type 'menu' for menu: \n\n"
+                    list_by_character(input)
+                    puts "Please enter another character or type 'menu' for menu: \n"
                 when "all"
-                    list_all_characters
-                    puts "Please enter another character or type 'menu' for menu: \n\n"
+                    list_by_all_name
+                    puts "Please enter another character or type 'menu' for menu: \n"
                 when "menu"
                     menu 
                 when "exit"
@@ -34,28 +68,5 @@ class Amiiboproject::CLI
             end
         end 
     end
-
-    #outputs the menu of options for the user
-    def menu
-        puts "Please pick your selection of characters below:"
-        puts "\n"
-        puts "1) Type 'zelda' for a list of Zelda related characters"
-        puts "2) Type 'mario' for a list of Mario related characters"
-        puts "3) Type 'zelda' for a list of Zelda related characters"
-        puts "4) Type 'all' for a list of All Amiibo characters"
-        puts "\n"
-        puts "Type exit to exit the program."
-    end
-
-
-    def gets_input(input)
-        input = gets.strip
-    end
-
-    #outputs the text to put in a valid option
-    def invalid_selection
-        puts "\nPlease enter a valid option! \n"
-    end     
-
  
 
