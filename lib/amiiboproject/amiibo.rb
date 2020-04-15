afbab65a1361bbe13fa9dde8863c2787630d4706
aredmond85 <aredmond85@gmail.com>
2020-04-15 15:45:56 -0500

    Find and Create now working.

--- lib/amiiboproject/amiibo.rb
+++ lib/amiiboproject/amiibo.rb
@@ -1,30 +1,28 @@
-#require 'pry'
 class Amiiboproject::Amiibo
 
+    # class variable that holds an array of all amiibo objects
     @@all = []   
 
+    # access elements of the class variable '@@all' array
     def self.all
         @@all
     end
 
-    def self.create(arrHash)
-        arrHash.each do |hash|
+    #create an instance of amiibo
+    def self.create(array)
+        array.each do |hash|
             new(hash["character"],hash["gameSeries"],hash["name"])
         end
     end
 
     def self.find_by_character(input)
-        #iterate through array
-        #find all amiibos that have the character that was inputted
-        @@all.find_all {|i| i.character == input.to_s}
+        @@all.find_all {|i| i.character.downcase == input}
     end
 
     attr_accessor :character,:gameSeries, :name
 
     def initialize(character, gameSeries, name)
-        @character = character
-        @gameSeries = gameSeries
-        @name = name
+        @character, @gameSeries, @name = character, gameSeries, name
         save
     end
 
--- lib/amiiboproject/cli.rb
+++ lib/amiiboproject/cli.rb
@@ -35,12 +35,14 @@ class Amiiboproject::CLI
         end 
     end
 
+    #outputs the menu of options for the user
     def menu
         puts "Please pick your selection of characters below:"
         puts "\n"
         puts "1) Type 'zelda' for a list of Zelda related characters"
         puts "2) Type 'mario' for a list of Mario related characters"
         puts "3) Type 'zelda' for a list of Zelda related characters"
+        puts "4) Type 'all' for a list of All Amiibo characters"
         puts "\n"
         puts "Type exit to exit the program."
     end
@@ -50,27 +52,10 @@ class Amiiboproject::CLI
         input = gets.strip
     end
 
-    # def valid?(input)
-    #    if input != "zelda" || "mario" || "all"
-    # end
-
+    #outputs the text to put in a valid option
     def invalid_selection
         puts "\nPlease enter a valid option! \n"
-    end
-
-    def list_selected_character
-        #iterate through amiibo array
-        #select the character
-        #output all instances of that amiibo character
-    end
-
-    def list_all_characters
-        Amiiboproject::Amiibo.all.each do |amiibo| 
-            puts "The character is: #{amiibo.character} \n"
-            puts "The character comes from this game series: #{amiibo.gameSeries} \n"
-            puts "The name of the figure is: #{amiibo.name} \n \n"
-        end
-    end      
+    end     
 
  
 
