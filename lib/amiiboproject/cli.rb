ca92ce8204ff817a1e62cbc22f632ebc7e9ad9d6
aredmond85 <aredmond85@gmail.com>
2020-04-15 14:09:47 -0500

    Updated instruction_loop method to include exit and new menu options.

--- lib/amiiboproject/cli.rb
+++ lib/amiiboproject/cli.rb
@@ -12,27 +12,37 @@ class Amiiboproject::CLI
     def instruction_loop
         menu
         input = nil 
-        while input != "exit" && !valid?
-            input = gets.downcase
+        while input != "exit" 
+            input = gets.strip.downcase
             case input
                 when "zelda"
                     list_selected_character
+                    puts "Please enter another character or type 'menu' for menu: \n\n"
                 when "mario"
                     list_selected_character
+                    puts "Please enter another character or type 'menu' for menu: \n\n"
                 when "all"
                     list_all_characters
+                    puts "Please enter another character or type 'menu' for menu: \n\n"
+                when "menu"
+                    menu 
+                when "exit"
+                    exit
                 else 
                     puts invalid_selection
+                end
             end
         end 
     end
 
     def menu
-        puts "Please pick your selection of characters below: \n"
-        puts "Type Zelda for Zelda characters"
-        puts "Type Mario for Mario characters"
-        puts "Type All for All Characters"
-        puts "Enter Selection below"
+        puts "Please pick your selection of characters below:"
+        puts "\n"
+        puts "1) Type 'zelda' for a list of Zelda related characters"
+        puts "2) Type 'mario' for a list of Mario related characters"
+        puts "3) Type 'zelda' for a list of Zelda related characters"
+        puts "\n"
+        puts "Type exit to exit the program."
     end
 
 
@@ -40,13 +50,12 @@ class Amiiboproject::CLI
         input = gets.strip
     end
 
-    def valid?(input)
-       if input != "zelda" || "mario" || "all"
-    end
+    # def valid?(input)
+    #    if input != "zelda" || "mario" || "all"
+    # end
 
     def invalid_selection
-        puts "I did not recognize that, please put in another character."
-        sleep(1)
+        puts "\nPlease enter a valid option! \n"
     end
 
     def list_selected_character
@@ -62,7 +71,6 @@ class Amiiboproject::CLI
             puts "The name of the figure is: #{amiibo.name} \n \n"
         end
     end      
-end
-end
+
  
 
