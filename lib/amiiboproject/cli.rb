c21a7b0787f87a3b820a1532beee12286e621902
aredmond85 <aredmond85@gmail.com>
2020-04-18 21:43:21 -0500

    Add continue method and colorize to CLI

--- lib/amiiboproject/cli.rb
+++ lib/amiiboproject/cli.rb
@@ -8,27 +8,39 @@ class Amiiboproject::CLI
 
     #outputs intro to the CLI project
     def intro
-        puts "\n\nWelcome to the Amiibo Selection!\n\n"
+        puts "\n\n#{"Welcome to the Amiibo Selection!".colorize(:cyan)}\n\n"
+        puts "Gathering all Amiibo Results...one moment..."
+        puts "-------------------------------------------\n\n"
+        sleep(1)
+    end
+
+    #creates amiibo objects, then runs browse_amiibos loop
+    def gather_amiibos
+        Amiiboproject::Amiibo.create(Amiiboproject::APIManager.get_amiibo)
+        browse_amiibos
     end
 
     #outputs the menu of options for the user
     def menu
-        puts "Type in a Nintendo character or Nintendo Series"
-        puts "- Type '1' for a list of all the Game Series included in the Amiibo collection"
-        puts "- Type '2' for a list of all the Amiibo Series included in the Amiibo collection"
-        puts "- Type '3' for a list of all the Characters included in Amiibo collection"
-        puts "Type 'exit' to exit the CLI"
-        puts "\n"
+        puts "How do you pronouce Amiibo? Hyphenate: A-me-bo\n\n"
+        puts "- Type in a #{"Nintendo Character".colorize(:red)} | #{"Game Series".colorize(:blue)} | #{"Amiibo Series".colorize(:green)}\n\n"
+        puts "- Type #{'1'.colorize(:yellow)} for a list of all the Game Series included in the Amiibo collection"
+        puts "- Type #{'2'.colorize(:yellow)} for a list of all the Amiibo Series included in the Amiibo collection"
+        puts "- Type #{'3'.colorize(:yellow)} for a list of all the Characters included in Amiibo collection\n\n"
+        puts "- Type #{'exit'.colorize(:yellow)} to exit the CLI\n\n"
+        puts "--------------------------------------------------------------------------------"
         sleep(2)
     end
 
-     #creates amiibo objects, then runs browse_amiibos loop
-     def gather_amiibos
-        puts "Gathering all Amiibo Results...one moment..."
-        sleep(1)
-        puts "\n"
-        Amiiboproject::Amiibo.create(Amiiboproject::APIManager.get_amiibo)
-        browse_amiibos
+    def continue(input)
+        puts "Would you like to continue?"
+        input = nil
+        input = gets.strip.downcase
+        if input == "yes"
+            menu
+        else
+            exit
+        end
     end
     
     #instruction loop for everything going on
@@ -39,98 +51,104 @@ class Amiiboproject::CLI
                 input = gets.strip.downcase
                 if input == "1"
                     list_all_gameseries
-                    second_instructions
+                    continue(input)
                 elsif input == "2"
                     list_all_amiiboseries
-                    second_instructions
+                    puts "\n\n"
+                    continue(input)
                 elsif input == "3"
                     list_all_character
-                    second_instructions
-                elsif input == "4"
-                    menu 
+                    continue(input)
                 elsif input == "exit"
                     exit
                 elsif character_valid?(input)
                     list_by_character(input)
-                    second_instructions
+                    continue(input)
                 elsif amiiboSeries_valid?(input)
                     list_by_amiiboSeries(input)
-                    second_instructions
-                # elsif gameSeries_valid?(input)
-                #     list_by_amiiboSeries(input)
-                #     second_instructions
+                    continue(input)
+                elsif gameSeries_valid?(input)
+                    list_by_gameSeries(input)
+                    continue(input)
                 else
-                    puts "Please enter a valid character."
+                    puts "\n\nThat selection does not exist. Please enter in another selection.\n\n"
                     browse_amiibos
                 end
             end
         end 
     end
-    #lists all amiibos by their gameSeries key
+
+    #lists all amiibos by their gameSeries key with no duplicates
     def list_all_gameseries
-        array = []
-        as = Amiiboproject::Amiibo.all.each {|x| array << x.gameSeries}
-        puts array.uniq
+        gameArray = []
+        Amiiboproject::Amiibo.all.each {|x| gameArray << x.gameSeries}
+        puts "-------------------------------------------\n\n"
+        puts gameArray.uniq
+        puts "-------------------------------------------\n\n"
     end
 
-    #lists all amiibos by their amiiboSeries key
+    #lists all amiibos by their amiiboSeries key with no duplicates
     def list_all_amiiboseries
-        array = []
-        as = Amiiboproject::Amiibo.all.each {|x| array << x.amiiboSeries}
-        puts array.uniq
+        amiiboArray = []
+        Amiiboproject::Amiibo.all.each {|x| amiiboArray << x.amiiboSeries}
+        puts "-------------------------------------------\n\n"
+        puts amiiboArray.uniq
+        puts "-------------------------------------------\n\n"
     end
 
-     #lists all amiibos by their character key
+     #lists all amiibos by their character key with no duplicates
     def list_all_character
-        array = []
-        as = Amiiboproject::Amiibo.all.each {|x| array << x.character}
-        puts array.uniq
+        characterArray = []
+        Amiiboproject::Amiibo.all.each {|x| characterArray << x.character}
+        puts "-------------------------------------------\n\n"
+        puts characterArray.uniq
+        puts "-------------------------------------------\n\n"
     end
 
      #selects and lists all amiibos by Character Series inputted
      def list_by_character(input)
-        as = Amiiboproject::Amiibo.all.select do |x| 
+        characterArray = Amiiboproject::Amiibo.all.select do |x| 
             x.name.downcase == input
         end
-        as.each.with_index(1) do |x, i|
+        characterArray.each.with_index(1) do |x, i|
             puts "Number #{i}"
             puts "-------------------------------------------"
-            puts "  -   Character Base Name: #{x.name.split.map(&:capitalize).join(' ')}"
-            puts "  -   Character Secondary Name: #{x.character.split.map(&:capitalize).join(' ')}"
-            puts "  -   Game Series: #{x.gameSeries.split.map(&:capitalize).join(' ')}"  
-            puts "  -   Amiibo Series: #{x.amiiboSeries.split.map(&:capitalize).join(' ')}"
+            puts "  -   Character Base Name: #{x.name.split.map(&:capitalize).join(' ').colorize(:yellow)}"
+            puts "  -   Character Secondary Name: #{x.character.split.map(&:capitalize).join(' ').colorize(:red)}"
+            puts "  -   Game Series: #{x.gameSeries.split.map(&:capitalize).join(' ').colorize(:blue)}"  
+            puts "  -   Amiibo Series: #{x.amiiboSeries.split.map(&:capitalize).join(' ').colorize(:green)}"
             puts "-------------------------------------------\n\n"   
         end
     end
 
     #selects and lists all amiibos by Character Series inputted
     def list_by_gameSeries(input)
-        as = Amiiboproject::Amiibo.all.select do |x| 
+        gameArray = Amiiboproject::Amiibo.all.select do |x| 
             x.gameSeries.downcase == input
         end
-        as.each.with_index(1) do |x, i|
+        gameArray.each.with_index(1) do |x, i|
             puts "Number #{i}"
             puts "-------------------------------------------"
-            puts "  -   Character Base Name: #{x.name.split.map(&:capitalize).join(' ')}"
-            puts "  -   Character Secondary Name: #{x.character.split.map(&:capitalize).join(' ')}"
-            puts "  -   Game Series: #{x.gameSeries.split.map(&:capitalize).join(' ')}"  
-            puts "  -   Amiibo Series: #{x.amiiboSeries.split.map(&:capitalize).join(' ')}"
+            puts "  -   Character Base Name: #{x.name.split.map(&:capitalize).join(' ').colorize(:yellow)}}"
+            puts "  -   Character Secondary Name: #{x.character.split.map(&:capitalize).join(' ').colorize(:red)}"
+            puts "  -   Game Series: #{x.gameSeries.split.map(&:capitalize).join(' ').colorize(:blue)}"  
+            puts "  -   Amiibo Series: #{x.amiiboSeries.split.map(&:capitalize).join(' ').colorize(:green)}"
             puts "-------------------------------------------\n\n"   
         end
     end
 
     #selects and lists all amiibos by Amiibo Series inputted
     def list_by_amiiboSeries(input)
-        as = Amiiboproject::Amiibo.all.select do |x| 
+        amiiboArray = Amiiboproject::Amiibo.all.select do |x| 
             x.amiiboSeries.downcase == input
         end
-        as.each.with_index(1) do |x, i|
+        amiiboArray.each.with_index(1) do |x, i|
             puts "Number #{i}"
             puts "-------------------------------------------"
-            puts "  -   Character Base Name: #{x.name.split.map(&:capitalize).join(' ')}"
-            puts "  -   Character Secondary Name: #{x.character.split.map(&:capitalize).join(' ')}"
-            puts "  -   Game Series: #{x.gameSeries.split.map(&:capitalize).join(' ')}"  
-            puts "  -   Amiibo Series: #{x.amiiboSeries.split.map(&:capitalize).join(' ')}"
+            puts "  -   Character Base Name: #{x.name.split.map(&:capitalize).join(' ').colorize(:yellow)}"
+            puts "  -   Character Secondary Name: #{x.character.split.map(&:capitalize).join(' ').colorize(:red)}"
+            puts "  -   Game Series: #{x.gameSeries.split.map(&:capitalize).join(' ').colorize(:blue)}"  
+            puts "  -   Amiibo Series: #{x.amiiboSeries.split.map(&:capitalize).join(' ').colorize(:green)}"
             puts "-------------------------------------------\n\n"   
         end
     end
@@ -146,14 +164,8 @@ class Amiiboproject::CLI
     end
 
     #validates if any Game Series keys exist within hash array
-    def gameSeries_valid?(amiiboSeries)
+    def gameSeries_valid?(gameSeries)
         Amiiboproject::Amiibo.all.any? { |amiibo| amiibo.gameSeries.downcase == gameSeries}
     end
 
-    #outputs secondary menu after the selection has run
-    def second_instructions
-        puts "  -   Enter another character"
-        puts "  -   Enter 'menu' to repeat the menu"
-        puts "  -   Enter an amiibo series you would like to see"
-        puts "  -   Enter 'exit' to exit the app \n\n"
-    end
+    #binding.pry
